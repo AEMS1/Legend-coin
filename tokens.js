909a899731173671599eb1cbcc1f6ca3b5636a9e
AEMS1 <aems91806@gmail.com>
2025-07-13 03:21:00 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,9 +1,8 @@
-// توکن‌هایی که خودت انتخاب می‌کنی فقط از اینجا قابل اضافه شدن هستند
 const tokenList = [
   { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955" },
   { symbol: "BUSD", address: "0xe9e7cea3dedca5984780bafc599bd69add087d56" },
+  { symbol: "WBNB", address: ethers.ZeroAddress }, // native BNB
   { symbol: "ETH", address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8" },
   { symbol: "BTCB", address: "0x7130d2a12b9bcBFAe4f2634d864A1Ee1Ce3Ead9c" },
-  { symbol: "CAKE", address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82" },
-  { symbol: "WBNB", address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" }
+  { symbol: "CAKE", address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82" }
 ];
