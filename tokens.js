6b3d75f7dc7df7e5d939f1466450f9f81ad3aa62
AEMS1 <aems91806@gmail.com>
2025-07-13 01:12:08 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -2,7 +2,7 @@ const tokenList = [
   {
     name: "BNB",
     symbol: "BNB",
-    address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
     decimals: 18
   },
   {
@@ -12,69 +12,21 @@ const tokenList = [
     decimals: 18
   },
   {
-    name: "USDC",
-    symbol: "USDC",
-    address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
-    decimals: 18
-  },
-  {
-    name: "BTCB",
-    symbol: "BTC",
-    address: "0x7130d2a12b9bcBFAe4f2634d864A1Ee1Ce3Ead9c",
+    name: "WBNB",
+    symbol: "WBNB",
+    address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     decimals: 18
   },
   {
     name: "ETH",
     symbol: "ETH",
-    address: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8",
     decimals: 18
   },
   {
-    name: "CAKE",
-    symbol: "CAKE",
-    address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
-    decimals: 18
-  },
-  {
-    name: "DAI",
-    symbol: "DAI",
-    address: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
-    decimals: 18
-  },
-  {
-    name: "XVS",
-    symbol: "XVS",
-    address: "0xCf6BB5389c92Bdda8a3747DdD24fAbEDbE0eC4d0",
-    decimals: 18
-  },
-  {
-    name: "LTC",
-    symbol: "LTC",
-    address: "0x4338665CBB7B2485A8855A139b75D5e34AB0DB94",
-    decimals: 18
-  },
-  {
-    name: "DOT",
-    symbol: "DOT",
-    address: "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402",
-    decimals: 18
-  },
-  {
-    name: "ADA",
-    symbol: "ADA",
-    address: "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47",
-    decimals: 18
-  },
-  {
-    name: "MATIC",
-    symbol: "MATIC",
-    address: "0xcc42724c6683b7e57334c4e856f4c9965ed682bd",
-    decimals: 18
-  },
-  {
-    name: "UNI",
-    symbol: "UNI",
-    address: "0xbf5140a22578168fd562dccf235e5d43a02ce9b1",
+    name: "BTCB",
+    symbol: "BTCB",
+    address: "0x7130d2a12b9bcBFAe4f2634d864A1Ee1Ce3Ead9c",
     decimals: 18
   }
 ];
