69e3aac199bae5a246d79ebd9742e81bc54e81dd
AEMS1 <aems91806@gmail.com>
2025-07-12 22:52:02 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,6 +1,12 @@
-
-const tokens = [
-    { symbol: "BNB", address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", decimals: 18 },
-    { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955", decimals: 18 },
-    { symbol: "BUSD", address: "0xe9e7cea3dedca5984780bafc599bd69add087d56", decimals: 18 }
+export const tokens = [
+  { symbol: "BNB", address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", decimals: 18 },
+  { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955", decimals: 18 },
+  { symbol: "BUSD", address: "0xe9e7cea3dedca5984780bafc599bd69add087d56", decimals: 18 },
+  { symbol: "BTCB", address: "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", decimals: 18 },
+  { symbol: "ETH",  address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8", decimals: 18 },
+  { symbol: "CAKE", address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", decimals: 18 },
+  { symbol: "ADA",  address: "0x3ee2200efb3400fabb9aacf31297cbdd1d435d47", decimals: 18 },
+  { symbol: "XRP",  address: "0x1d2f0da169ceb9fc7c8e3f6c2b3e5a3e6a5d54b8", decimals: 18 },
+  { symbol: "DOT",  address: "0x7083609fce4d1d8dc0c979aab8c869ea2c873402", decimals: 18 },
+  { symbol: "LINK", address: "0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd", decimals: 18 }
 ];
