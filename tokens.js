710ff5fece5e6c0aa127d95cd0f4fe28d61336df
AEMS1 <aems91806@gmail.com>
2025-07-13 01:23:31 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -2,7 +2,7 @@ const tokenList = [
   {
     name: "BNB",
     symbol: "BNB",
-    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // For native BNB
     decimals: 18
   },
   {
@@ -15,18 +15,6 @@ const tokenList = [
     name: "WBNB",
     symbol: "WBNB",
     address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-    decimals: 18
-  },
-  {
-    name: "ETH",
-    symbol: "ETH",
-    address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8",
-    decimals: 18
-  },
-  {
-    name: "BTCB",
-    symbol: "BTCB",
-    address: "0x7130d2a12b9bcBFAe4f2634d864A1Ee1Ce3Ead9c",
     decimals: 18
   }
 ];
