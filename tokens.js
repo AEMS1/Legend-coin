91cf8f9ab758bd53e57d7ef401901955c1a3d8e0
AEMS1 <aems91806@gmail.com>
2025-07-13 00:22:36 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,12 +1,80 @@
-export const tokens = [
-  { symbol: "BNB", address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", decimals: 18 },
-  { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955", decimals: 18 },
-  { symbol: "BUSD", address: "0xe9e7cea3dedca5984780bafc599bd69add087d56", decimals: 18 },
-  { symbol: "BTCB", address: "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", decimals: 18 },
-  { symbol: "ETH",  address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8", decimals: 18 },
-  { symbol: "CAKE", address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", decimals: 18 },
-  { symbol: "ADA",  address: "0x3ee2200efb3400fabb9aacf31297cbdd1d435d47", decimals: 18 },
-  { symbol: "XRP",  address: "0x1d2f0da169ceb9fc7c8e3f6c2b3e5a3e6a5d54b8", decimals: 18 },
-  { symbol: "DOT",  address: "0x7083609fce4d1d8dc0c979aab8c869ea2c873402", decimals: 18 },
-  { symbol: "LINK", address: "0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd", decimals: 18 }
+const tokenList = [
+  {
+    name: "BNB",
+    symbol: "BNB",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    decimals: 18
+  },
+  {
+    name: "USDT",
+    symbol: "USDT",
+    address: "0x55d398326f99059fF775485246999027B3197955",
+    decimals: 18
+  },
+  {
+    name: "USDC",
+    symbol: "USDC",
+    address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+    decimals: 18
+  },
+  {
+    name: "BTCB",
+    symbol: "BTC",
+    address: "0x7130d2a12b9bcBFAe4f2634d864A1Ee1Ce3Ead9c",
+    decimals: 18
+  },
+  {
+    name: "ETH",
+    symbol: "ETH",
+    address: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    decimals: 18
+  },
+  {
+    name: "CAKE",
+    symbol: "CAKE",
+    address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
+    decimals: 18
+  },
+  {
+    name: "DAI",
+    symbol: "DAI",
+    address: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
+    decimals: 18
+  },
+  {
+    name: "XVS",
+    symbol: "XVS",
+    address: "0xCf6BB5389c92Bdda8a3747DdD24fAbEDbE0eC4d0",
+    decimals: 18
+  },
+  {
+    name: "LTC",
+    symbol: "LTC",
+    address: "0x4338665CBB7B2485A8855A139b75D5e34AB0DB94",
+    decimals: 18
+  },
+  {
+    name: "DOT",
+    symbol: "DOT",
+    address: "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402",
+    decimals: 18
+  },
+  {
+    name: "ADA",
+    symbol: "ADA",
+    address: "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47",
+    decimals: 18
+  },
+  {
+    name: "MATIC",
+    symbol: "MATIC",
+    address: "0xcc42724c6683b7e57334c4e856f4c9965ed682bd",
+    decimals: 18
+  },
+  {
+    name: "UNI",
+    symbol: "UNI",
+    address: "0xbf5140a22578168fd562dccf235e5d43a02ce9b1",
+    decimals: 18
+  }
 ];
