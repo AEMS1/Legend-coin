4434787a25596511ad416f969b227e18ce99a852
AEMS1 <aems91806@gmail.com>
2025-07-13 00:24:52 +0330

    Delete abi.js

--- abi.js
+++
@@ -1,7 +0,0 @@
-export const erc20Abi = [
-  "function approve(address spender, uint256 amount) external returns (bool)",
-  "function decimals() view returns (uint8)",
-  "function symbol() view returns (string)",
-  "function transferFrom(address from, address to, uint amount) returns (bool)",
-  "function balanceOf(address account) view returns (uint)"
-];
