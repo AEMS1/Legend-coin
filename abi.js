4c09292cba53b6111cac70f29455040f07ae61a5
AEMS1 <aems91806@gmail.com>
2025-07-13 01:24:29 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,13 +1,12 @@
 const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; // PancakeSwap V2
 const routerAbi = [
   "function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts)",
-  "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
-  "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) payable external returns (uint[] memory amounts)"
+  "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) payable external returns (uint[] memory amounts)",
+  "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)"
 ];
 
 const erc20Abi = [
   "function approve(address spender, uint amount) external returns (bool)",
   "function decimals() view returns (uint8)",
-  "function balanceOf(address account) view returns (uint256)",
-  "function allowance(address owner, address spender) view returns (uint256)"
+  "function transfer(address to, uint amount) external returns (bool)"
 ];
