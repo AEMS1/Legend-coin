e7e3c862bcf3c82dd918ef158b3563e7e9b7a941
AEMS1 <aems91806@gmail.com>
2025-07-13 09:05:47 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,6 +1,35 @@
 const pancakeRouterABI = [
   {
     "inputs": [
+      { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" },
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "deadline", "type": "uint256" }
+    ],
+    "name": "swapExactETHForTokens",
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "amountIn", "type": "uint256" },
+      { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" },
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "deadline", "type": "uint256" }
+    ],
+    "name": "swapExactTokensForETH",
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
       { "internalType": "uint256", "name": "amountIn", "type": "uint256" },
       { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
       { "internalType": "address[]", "name": "path", "type": "address[]" },
@@ -8,7 +37,9 @@ const pancakeRouterABI = [
       { "internalType": "uint256", "name": "deadline", "type": "uint256" }
     ],
     "name": "swapExactTokensForTokens",
-    "outputs": [{ "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }],
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
     "stateMutability": "nonpayable",
     "type": "function"
   },
@@ -18,7 +49,9 @@ const pancakeRouterABI = [
       { "internalType": "address[]", "name": "path", "type": "address[]" }
     ],
     "name": "getAmountsOut",
-    "outputs": [{ "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }],
+    "outputs": [
+      { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }
+    ],
     "stateMutability": "view",
     "type": "function"
   }
