3fdccc969e7e0223ab543ad189b947364ec9cc83
AEMS1 <aems91806@gmail.com>
2025-07-13 04:43:14 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,7 +1,20 @@
-const tokenList = [
-  { symbol: "WBNB", address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" },
-  { symbol: "BUSD", address: "0xe9e7cea3dedca5984780bafc599bd69add087d56" },
-  { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955" },
-  { symbol: "BTCB", address: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c" },
-  { symbol: "ETH",  address: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8" }
+const tokens = [
+  {
+    name: "BNB",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    symbol: "BNB",
+    decimals: 18
+  },
+  {
+    name: "LGD",
+    address: "0x4751C0DE56EFB3770615097347cbF131D302498A",
+    symbol: "LGD",
+    decimals: 18
+  },
+  {
+    name: "USDT",
+    address: "0x55d398326f99059fF775485246999027B3197955",
+    symbol: "USDT",
+    decimals: 18
+  }
 ];
